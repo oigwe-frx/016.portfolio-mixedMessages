ea5ad4762f41d547afce8459a392b6e74192c092
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-08-25 13:51:28 -0400

    Correct faulty set timeOut function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {messageBuilder} from './tarotLibrary.js';
 //DOM Manipulation
 const btnRefreshIcon = document.getElementById('js-refresh');
 const btnRefresh = document.getElementById('do-over')
-const subtitle = document.getElementById('subtitle');
+const subtitle = document.getElementById('js-subtitle');
  window.onload = function () {
   const cardData = messageBuilder();
  
@@ -13,8 +13,17 @@ const subtitle = document.getElementById('subtitle');
   }
   
   let i = 0;
+    window.onload = function () {
+  const cardData = messageBuilder();
+
+
+  btnRefreshIcon.onclick = function () {
+    location.reload()
+  }
+
+  let i = 0;
     setTimeout(function(){ 
-     
+
       while(i < 3){
         let card = 'card-'+`${i}`;
         let back = 'back-'+`${i}`;
@@ -22,10 +31,12 @@ const subtitle = document.getElementById('subtitle');
         document.getElementById(back).innerHTML = `<img src="${cardData[i].img}"/>`;
         i++;
       }
-    
+
       clearInterval();
      }, 2000);
+
      subtitle.classList.toggle('visible');
+
      setTimeout(function(){ 
        let html = ''
        let i = 0;
@@ -34,6 +45,8 @@ const subtitle = document.getElementById('subtitle');
         i++;
       }
       btnRefresh.classList.toggle('visible');
+
+
        document.getElementById('message').innerHTML = `<section class="grey row align-items-center" style="padding: 5%;">
         <div class="col">
           <div class="row">
@@ -42,7 +55,7 @@ const subtitle = document.getElementById('subtitle');
           </div>
         </div>
         </section>`
-      
+
       clearInterval();
      }, 6000);
   };
