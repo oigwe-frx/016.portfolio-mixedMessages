9505018a77f6ef96f8d6c20884dac77480144bdd
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-08-25 12:05:43 -0400

    Revert code to before id changes

--- index.js
+++ index.js
@@ -2,55 +2,47 @@ import {messageBuilder} from './tarotLibrary.js';
 
 
 //DOM Manipulation
-
 const btnRefreshIcon = document.getElementById('js-refresh');
-const btnRefresh = document.getElementById('do-over');
-const subtitle = document.getElementById('js-subtitle');
-
-window.onload = function() {
+const btnRefresh = document.getElementById('do-over')
+const subtitle = document.getElementById('subtitle');
+ window.onload = function () {
   const cardData = messageBuilder();
-
-  btnRefreshIcon.onclick = function() {
-    location.reload();
-  };
-
+ 
+  btnRefreshIcon.onclick = function () {
+    location.reload()
+  }
+  
   let i = 0;
-  setTimeout(function() {
-    while (i < 3) {
-      let card = 'js-card-' + `${i}`;
-      let cardface = 'js-cardFace-' + `${i}`;
-      document.getElementById(card).classList.toggle('flipCard');
-      document.getElementById(cardface).innerHTML = `<img src="${
-        cardData[i].img
-      }"/>`;
-      i++;
-    }
-
-    clearInterval();
-  }, 2000);
-
-  subtitle.classList.toggle('visible');
-
-  setTimeout(function() {
-    let html = '';
-    let i = 0;
-    while (i < 3) {
-      html += cardData[i].message;
-      i++;
-    }
-    btnRefresh.classList.toggle('visible');
-
-    document.getElementById(
-      'message'
-    ).innerHTML = `<section class="grey row align-items-center" style="padding: 5%;">
+    setTimeout(function(){ 
+     
+      while(i < 3){
+        let card = 'card-'+`${i}`;
+        let back = 'back-'+`${i}`;
+        document.getElementById(card).classList.toggle('flipCard');
+        document.getElementById(back).innerHTML = `<img src="${cardData[i].img}"/>`;
+        i++;
+      }
+    
+      clearInterval();
+     }, 2000);
+     subtitle.classList.toggle('visible');
+     setTimeout(function(){ 
+       let html = ''
+       let i = 0;
+      while(i < 3){
+        html += cardData[i].message;
+        i++;
+      }
+      btnRefresh.classList.toggle('visible');
+       document.getElementById('message').innerHTML = `<section class="grey row align-items-center" style="padding: 5%;">
         <div class="col">
           <div class="row">
             <!--Card Data-->
             ${html}
           </div>
         </div>
-        </section>`;
-
-    clearInterval();
-  }, 6000);
-};
+        </section>`
+      
+      clearInterval();
+     }, 6000);
+  };
