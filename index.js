90b4dfea1923b468af59b8b420d9bd26e172a671
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-08-25 14:11:01 -0400

    Revert code back 12hrs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {messageBuilder} from './tarotLibrary.js';
 //DOM Manipulation
 const btnRefreshIcon = document.getElementById('js-refresh');
 const btnRefresh = document.getElementById('do-over')
-const subtitle = document.getElementById('js-subtitle');
+const subtitle = document.getElementById('subtitle');
  window.onload = function () {
   const cardData = messageBuilder();
  
@@ -13,17 +13,8 @@ const subtitle = document.getElementById('js-subtitle');
   }
   
   let i = 0;
-    window.onload = function () {
-  const cardData = messageBuilder();
-
-
-  btnRefreshIcon.onclick = function () {
-    location.reload()
-  }
-
-  let i = 0;
     setTimeout(function(){ 
-
+     
       while(i < 3){
         let card = 'card-'+`${i}`;
         let back = 'back-'+`${i}`;
@@ -31,12 +22,10 @@ const subtitle = document.getElementById('js-subtitle');
         document.getElementById(back).innerHTML = `<img src="${cardData[i].img}"/>`;
         i++;
       }
-
+    
       clearInterval();
      }, 2000);
-
      subtitle.classList.toggle('visible');
-
      setTimeout(function(){ 
        let html = ''
        let i = 0;
@@ -45,8 +34,6 @@ const subtitle = document.getElementById('js-subtitle');
         i++;
       }
       btnRefresh.classList.toggle('visible');
-
-
        document.getElementById('message').innerHTML = `<section class="grey row align-items-center" style="padding: 5%;">
         <div class="col">
           <div class="row">
@@ -55,7 +42,7 @@ const subtitle = document.getElementById('js-subtitle');
           </div>
         </div>
         </section>`
-
+      
       clearInterval();
      }, 6000);
   };
