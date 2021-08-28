2b47e0d6131ed8633efc2544c401e5d086024ee0
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-08-28 16:24:31 -0400

    Change fetch function to async

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ import { messageBuilder } from './tarotLibrary.js';
 
 
 window.onload = function () {
- 
-// JSON REQUEST FOR TAROT DATA
 
-  fetch('./data/tarotCards.json')
-    .then(response => {
-      return response.json();
-    })
+
+
+  // JSON REQUEST FOR TAROT DATA
+
+  async function fetchTarotData() {
+    let response = await fetch('./data/tarotCards.json');
+
+    if (!response.ok) {
+      document.getElementById('js-message').innerHTML = `<h2>${response.status}</h2>`
+    }
+
+    return await response.json();
+  }
+
+  fetchTarotData()
     .then((data) => {
-
-    //MESSAGEBUILDER FUNCTION WILL PARSE JSON OBJECT DATA AND BUILD THE READING  
+          
+      //MESSAGEBUILDER FUNCTION WILL PARSE JSON OBJECT DATA AND BUILD THE READING  
       const cardData = messageBuilder(data);
 
-      
-    //SET TIMEOUTS PRESENT TAROT DATA TO USER
+      //SET TIMEOUTS PRESENT TAROT DATA TO USER
       //TIMEOUT (1) FLIPS CARDS FROM BACK TO FRONT PRESENTING TAROT CARD IMAGES
       setTimeout(function () {
 
@@ -27,45 +35,52 @@ window.onload = function () {
         while (i < 3) {
           let card = 'js-card-' + `${i}`;
           let cardFace = 'js-card-face-' + `${i}`;
-          
-          document.getElementById(card).classList.toggle('flip-card');
+
           document.getElementById(cardFace).innerHTML = `<img src="${cardData[i].img}"/>`;
+          document.getElementById(card).classList.toggle('flip-card');
           i++;
         }
 
         clearInterval();
       }, 2000);
 
-      
-     //TIMEOUT (2) CONTROLS THE STEPS NEEDED TO DISPLAY THE TAROT READING MESSAGES TO THE USER
+      return cardData;
+
+    })
+    .then((data) => {
+      const cardData = data;
+
+      //TIMEOUT (2) CONTROLS THE STEPS NEEDED TO DISPLAY THE TAROT READING MESSAGES TO THE USER
       setTimeout(function () {
-        
-       //SUBTITLE BANNER DISAPPEARS
+
         const subtitle = document.getElementById('js-subtitle');
+        const refresh = document.getElementById('js-refresh');
+        const btnRefreshIcon = document.getElementById('js-refresh-icon');
+
+
+        //SUBTITLE BANNER DISAPPEARS
         subtitle.classList.toggle('visible');
 
-       
-       //REFRESH BUTTON APPEARS AND REFRESH FUNCTION IS ATTACHED TO BUTTON
-        const refresh = document.getElementById('js-refresh')
+        //REFRESH BUTTON APPEARS AND REFRESH FUNCTION IS ATTACHED TO BUTTON
         refresh.classList.toggle('visible');
-        const btnRefreshIcon = document.getElementById('js-refresh-icon');
         btnRefreshIcon.onclick = function () {
-         location.reload()
+          location.reload()
         }
-       
-       //DOM MANIPULATION TO DISPLAY THE MESSAGE
+
+
+        //DOM MANIPULATION TO DISPLAY THE MESSAGE
         let html = ''
         let i = 0;
-        
+
         while (i < 3) {
           let cardBack = 'js-card-back-' + `${i}`
           document.getElementById(cardBack).classList.toggle('visible');
 
           html += cardData[i].message;
+          console.log(html)
           i++;
         }
-        
-        
+
         document.getElementById('js-message').innerHTML = `<section class="row grey">
         <div class="col">
           <div class="row">
@@ -77,8 +92,7 @@ window.onload = function () {
 
         clearInterval();
       }, 6000);
+
     });
 
-
-
-};
+}
