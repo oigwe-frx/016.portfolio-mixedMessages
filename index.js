827478b90101ece10ef8d3f5ce0c28ec083df863
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-08-27 23:17:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
-import {messageBuilder} from './tarotLibrary.js';
+import { messageBuilder } from './tarotLibrary.js';
 
 
 //DOM Manipulation
-const btnRefreshIcon = document.getElementById('js-refresh');
-const btnRefresh = document.getElementById('do-over')
-const subtitle = document.getElementById('subtitle');
- window.onload = function () {
-  const cardData = messageBuilder();
+
+
+window.onload = function () {
  
-  btnRefreshIcon.onclick = function () {
-    location.reload()
-  }
-  
-  let i = 0;
-    setTimeout(function(){ 
-     
-      while(i < 3){
-        let card = 'card-'+`${i}`;
-        let back = 'back-'+`${i}`;
-        document.getElementById(card).classList.toggle('flipCard');
-        document.getElementById(back).innerHTML = `<img src="${cardData[i].img}"/>`;
-        i++;
-      }
-    
-      clearInterval();
-     }, 2000);
-     subtitle.classList.toggle('visible');
-     setTimeout(function(){ 
-       let html = ''
-       let i = 0;
-      while(i < 3){
-        html += cardData[i].message;
-        i++;
-      }
-      btnRefresh.classList.toggle('visible');
-       document.getElementById('message').innerHTML = `<section class="grey row align-items-center" style="padding: 5%;">
+// JSON REQUEST FOR TAROT DATA
+
+  fetch('./data/tarotCards.json')
+    .then(response => {
+      return response.json();
+    })
+    .then((data) => {
+
+    //MESSAGEBUILDER FUNCTION WILL PARSE JSON OBJECT DATA AND BUILD THE READING  
+      const cardData = messageBuilder(data);
+
+      
+    //SET TIMEOUTS PRESENT TAROT DATA TO USER
+      //TIMEOUT (1) FLIPS CARDS FROM BACK TO FRONT PRESENTING TAROT CARD IMAGES
+      setTimeout(function () {
+
+        let i = 0;
+
+        while (i < 3) {
+          let card = 'js-card-' + `${i}`;
+          let cardFace = 'js-card-face-' + `${i}`;
+          
+          document.getElementById(card).classList.toggle('flip-card');
+          document.getElementById(cardFace).innerHTML = `<img src="${cardData[i].img}"/>`;
+          i++;
+        }
+
+        clearInterval();
+      }, 2000);
+
+      
+     //TIMEOUT (2) CONTROLS THE STEPS NEEDED TO DISPLAY THE TAROT READING MESSAGES TO THE USER
+      setTimeout(function () {
+        
+       //SUBTITLE BANNER DISAPPEARS
+        const subtitle = document.getElementById('js-subtitle');
+        subtitle.classList.toggle('visible');
+
+       
+       //REFRESH BUTTON APPEARS AND REFRESH FUNCTION IS ATTACHED TO BUTTON
+        const refresh = document.getElementById('js-refresh')
+        refresh.classList.toggle('visible');
+        const btnRefreshIcon = document.getElementById('js-refresh-icon');
+        btnRefreshIcon.onclick = function () {
+         location.reload()
+        }
+       
+       //DOM MANIPULATION TO DISPLAY THE MESSAGE
+        let html = ''
+        let i = 0;
+        
+        while (i < 3) {
+          html += cardData[i].message;
+          i++;
+        }
+        
+        
+        document.getElementById('js-message').innerHTML = `<section class="row grey">
         <div class="col">
           <div class="row">
             <!--Card Data-->
@@ -42,7 +71,11 @@ const subtitle = document.getElementById('subtitle');
           </div>
         </div>
         </section>`
-      
-      clearInterval();
-     }, 6000);
-  };
+
+        clearInterval();
+      }, 6000);
+    });
+
+
+
+};
